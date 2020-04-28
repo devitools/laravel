7ad15b80dde98a524aeb07899bd31c450e2b27b0
William Correa <wilcorrea@gmail.com>
2020-04-27 21:13:51 -0300

    [feature] Add audit parser

---
+++ Audit/Model.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace DeviTools\Audit;
+
+use Illuminate\Database\Eloquent\JsonEncodingException;
+use OwenIt\Auditing\Models\Audit;
+use DeviTools\Persistence\AbstractModel as Common;
+use Throwable;
+
+use function DeviTools\Helper\is_binary;
+
+/**
+ * Class Model
+ *
+ * @package App\Audit
+ */
+class Model extends Audit
+{
+    /**
+     * Cast the given attribute to JSON.
+     *
+     * @param string $key
+     * @param mixed $value
+     *
+     * @return string
+     */
+    protected function castAttributeAsJson($key, $value)
+    {
+        try {
+            foreach ($value as $index => $content) {
+                if (!is_binary($content)) {
+                    continue;
+                }
+                $value[$index] = Common::decodeUuid($content);
+            }
+
+            $value = $this->asJson($value);
+
+            if ($value === false) {
+                throw JsonEncodingException::forAttribute(
+                    $this,
+                    $key,
+                    json_last_error_msg()
+                );
+            }
+
+            return $value;
+        } catch (Throwable $error) {
+            return null;
+        }
+    }
+}
