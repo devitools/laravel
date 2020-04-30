bc6a5cbe32a108c34f5fe6cc91b7f47a74baa61b
William Correa <wilcorrea@gmail.com>
2020-04-30 13:08:10 -0300

    [feature] Normalize JSON response to utf8

--- Http/Response/Answer.php
+++ Http/Response/Answer.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace DeviTools\Http\Response;
 
 use DeviTools\Http\Status;
+use ForceUTF8\Encoding;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Response;
+use Throwable;
 
 /**
  * Class Answer
@@ -22,14 +24,14 @@ abstract class Answer
      *
      * @return JsonResponse
      */
-    public static function success($data, array $meta, int $code = Status::CODE_200)
+    public static function success($data, array $meta, int $code = Status::CODE_200): JsonResponse
     {
         $response = [
             'status' => 'success',
             'data' => $data,
             'meta' => $meta,
         ];
-        return Response::json($response, $code);
+        return static::response($response, $code);
     }
 
     /**
@@ -39,14 +41,14 @@ abstract class Answer
      *
      * @return JsonResponse
      */
-    public static function fail($data, array $meta, int $code = Status::CODE_400)
+    public static function fail($data, array $meta, int $code = Status::CODE_400): JsonResponse
     {
         $response = [
             'status' => 'fail',
             'data' => $data,
             'meta' => $meta,
         ];
-        return Response::json($response, $code);
+        return static::response($response, $code);
     }
 
     /**
@@ -58,8 +60,13 @@ abstract class Answer
      *
      * @return JsonResponse
      */
-    public static function error($message, $code = Status::CODE_500, $meta = null, $commit = null, $debug = null)
-    {
+    public static function error(
+        $message,
+        $code = Status::CODE_500,
+        $meta = null,
+        $commit = null,
+        $debug = null
+    ): JsonResponse {
         $response = [
             'commit' => $commit,
             'status' => 'error',
@@ -78,6 +85,26 @@ abstract class Answer
             $response['$debug'] = $debug;
         }
 
-        return Response::json($response, $code);
+        return static::response($response, $code);
+    }
+
+    /**
+     * @param array $data
+     * @param int $code
+     *
+     * @return JsonResponse
+     */
+    protected static function response(array $data, int $code): JsonResponse
+    {
+        try {
+            return Response::json($data, $code);
+        } catch (Throwable $throwable) {
+        }
+        array_walk_recursive($data, static function (&$item) {
+            if (!mb_detect_encoding((string)$item, 'utf-8', true)) {
+                $item = Encoding::fixUTF8($item);
+            }
+        });
+        return Response::json($data, $code);
     }
 }
