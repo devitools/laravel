a1d2452a66abac04990fafeec6f6b1673db1b924
William Correa <wilcorrea@gmail.com>
2021-06-22 11:27:51 -0300

    [feature] Add date filter

--- src/Persistence/Filter/Filters.php
+++ src/Persistence/Filter/Filters.php
@@ -6,6 +6,7 @@ namespace Devitools\Persistence\Filter;
 
 use Devitools\Persistence\Filter\Operators\FilterBetween;
 use Devitools\Persistence\Filter\Operators\FilterCurrency;
+use Devitools\Persistence\Filter\Operators\FilterDate;
 use Devitools\Persistence\Filter\Operators\FilterEqual;
 use Devitools\Persistence\Filter\Operators\FilterIn;
 use Devitools\Persistence\Filter\Operators\FilterLike;
@@ -29,27 +30,7 @@ final class Filters
     /**
      * @var string
      */
-    public const EQUAL = 'eq';
-
-    /**
-     * @var string
-     */
-    public const NOT_EQUAL = 'neq';
-
-    /**
-     * @var string
-     */
-    public const LIKE = 'like';
-
-    /**
-     * @var string
-     */
-    public const IN = 'in';
-
-    /**
-     * @var string
-     */
-    public const NIN = 'nin';
+    public const BETWEEN = 'between';
 
     /**
      * @var string
@@ -59,7 +40,22 @@ final class Filters
     /**
      * @var string
      */
-    public const BETWEEN = 'between';
+    public const DATE = 'date';
+
+    /**
+     * @var string
+     */
+    public const EQUAL = 'eq';
+
+    /**
+     * @var string
+     */
+    public const IN = 'in';
+
+    /**
+     * @var string
+     */
+    public const LIKE = 'like';
 
     /**
      * @var string
@@ -70,6 +66,16 @@ final class Filters
      * @var string
      */
     public const MULTI_LIKE = 'multi-like';
+
+    /**
+     * @var string
+     */
+    public const NOT_EQUAL = 'neq';
+
+    /**
+     * @var string
+     */
+    public const NOT_IN = 'nin';
 
     /**
      * @var array
@@ -95,15 +101,16 @@ final class Filters
         }
 
         $operators = [
-            self::EQUAL => FilterEqual::class,
-            self::NOT_EQUAL => FilterNotEqual::class,
-            self::LIKE => FilterLike::class,
-            self::IN => FilterIn::class,
-            self::NIN => FilterNotIn::class,
-            self::CURRENCY => FilterCurrency::class,
             self::BETWEEN => FilterBetween::class,
+            self::CURRENCY => FilterCurrency::class,
+            self::DATE => FilterDate::class,
+            self::EQUAL => FilterEqual::class,
+            self::IN => FilterIn::class,
+            self::LIKE => FilterLike::class,
             self::MULTI_EQUAL => FilterMultiEqual::class,
             self::MULTI_LIKE => FilterMultiLike::class,
+            self::NOT_EQUAL => FilterNotEqual::class,
+            self::NOT_IN => FilterNotIn::class,
         ];
 
         if (!isset($operators[$operator])) {
@@ -115,5 +122,117 @@ final class Filters
         self::$filters[$operator] = $reference::get();
 
         return self::$filters[$operator];
+    }
+
+    /**
+     * @param string $value
+     * @param string $filter
+     *
+     * @return string
+     */
+    public static function filterAs(string $value, string $filter): string
+    {
+        $separator = self::SEPARATION_OPERATOR;
+        return "{$filter}{$separator}{$value}";
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return string
+     */
+    public static function filterBetween(string $value): string
+    {
+        return self::filterAs($value, self::BETWEEN);
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return string
+     */
+    public static function filterCurrency(string $value): string
+    {
+        return self::filterAs($value, self::CURRENCY);
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return string
+     */
+    public static function filterDate(string $value): string
+    {
+        return self::filterAs($value, self::DATE);
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return string
+     */
+    public static function filterEqual(string $value): string
+    {
+        return self::filterAs($value, self::EQUAL);
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return string
+     */
+    public static function filterIn(string $value): string
+    {
+        return self::filterAs($value, self::IN);
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return string
+     */
+    public static function filterLike(string $value): string
+    {
+        return self::filterAs($value, self::LIKE);
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return string
+     */
+    public static function filterMultiEqual(string $value): string
+    {
+        return self::filterAs($value, self::MULTI_EQUAL);
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return string
+     */
+    public static function filterMultiLike(string $value): string
+    {
+        return self::filterAs($value, self::MULTI_LIKE);
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return string
+     */
+    public static function filterNotEqual(string $value): string
+    {
+        return self::filterAs($value, self::NOT_EQUAL);
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return string
+     */
+    public static function filterNotIn(string $value): string
+    {
+        return self::filterAs($value, self::NOT_IN);
     }
 }
