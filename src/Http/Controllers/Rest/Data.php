0e0ccd3207ac1ff3a0d03aa630225e81102a7b58
William Correa <wilcorrea@gmail.com>
2021-07-11 19:43:51 -0300

    (refactor) small adjustments to trait data of rest

--- src/Http/Controllers/Rest/Data.php
+++ src/Http/Controllers/Rest/Data.php
@@ -1,13 +1,23 @@
 <?php
 
+declare(strict_types=1);
 
 namespace Devitools\Http\Controllers\Rest;
 
-
 use Illuminate\Http\Request;
 
+/**
+ * Trait Data
+ *
+ * @package Devitools\Http\Controllers\Rest
+ */
 trait Data
 {
+    /**
+     * @param Request $request
+     *
+     * @return array
+     */
     protected function getData(Request $request): array
     {
         return $request->all();
