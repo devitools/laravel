8005d0663a56f37458f4623f7fe29f9e961ae9aa
Wiliam Correa <wilcorrea@gmail.com>
2022-06-07 21:35:49 -0300

    feature: add event validation on add event

--- src/Agnostic/Events.php
+++ src/Agnostic/Events.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Devitools\Agnostic;
 
+use Devitools\Exceptions\ErrorRuntime;
+
 /**
  * Trait Events
  *
@@ -15,10 +17,30 @@ trait Events
      * @param string $event
      * @param string $handler
      *
-     * @return $this
+     * @return Events|Schema
+     * @throws ErrorRuntime
      */
     protected function addEvent(string $event, string $handler): self
     {
+        $supported = [
+            'retrieved',
+            'creating',
+            'created',
+            'updating',
+            'updated',
+            'saving',
+            'saved',
+            'deleting',
+            'deleted',
+            'trashed',
+            'forceDeleted',
+            'restoring',
+            'restored',
+            'replicating',
+        ];
+        if (!in_array($event, $supported)) {
+            throw new ErrorRuntime(['event' => 'not-supported']);
+        }
         $this->dispatchesEvents[$event] = $handler;
         return $this;
     }
